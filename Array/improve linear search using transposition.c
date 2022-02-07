89f6cf8aef509948e79ae2cdfe2ce0b6ad47b00f
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-02-07 07:23:53 -0800

    improve linear search using transposition

---
+++ Array/improve linear search using transposition.c
@@ -0,0 +1,45 @@
+
+#include<stdio.h>
+struct Array
+{
+    int A[10];
+    int length;
+    int size;
+};
+void print(struct Array arr)
+{
+    int i;
+    printf("The given array is: ");
+    for(i=0; i<arr.length; i++)
+    {
+
+        printf("%d ",arr.A[i]);
+    }
+}
+void swap(int *x,int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int Linearsearch(struct Array *arr,int key)
+{
+    int i;
+    for(i=0; i<arr->length; i++)
+    {
+        if(key == arr->A[i])
+        {
+            swap(&arr->A[i],&arr->A[i-1]);
+            return i+1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    struct Array arr = {{2,4,5,6,8,3},6,10};
+    printf("The searching value is in %d place\n",Linearsearch(&arr,8));
+    print(arr);
+
+}
