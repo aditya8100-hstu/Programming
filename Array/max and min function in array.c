34e26715d0e81a823fcf41321abcd96e101b85ad
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-02-09 08:06:06 -0800

    max and min operation in an array

---
+++ Array/max and min function in array.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+struct Array
+{
+    int A[10];
+    int length;
+    int size;
+};
+
+int print(struct Array arr)
+{
+    int i;
+    printf("The new array elements are: ");
+    for(i=0; i<arr.length; i++)
+        printf("%d ",arr.A[i]);
+}
+int maxfunction(struct Array arr)
+{
+    int i;
+    int max = arr.A[0];
+    for(i=1; i<arr.length; i++)
+    {
+        if(max<arr.A[i])
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+int minfunction(struct Array arr)
+{
+    int i;
+    int max = arr.A[0];
+    for(i=1; i<arr.length; i++)
+    {
+        if(max>arr.A[i])
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    struct Array arr = {{2,10,7,5,3,4},6,10};
+    printf("The maximum array element is : %d\n",maxfunction(arr));
+     printf("The maximum array element is : %d\n",minfunction(arr));
+    print(arr);
+}
