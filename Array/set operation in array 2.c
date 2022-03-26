0a2dc5256c504a55cb95cf2e17628ae803927b1e
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-26 21:05:01 +0600

    set operation in an array

--- Array/set operation in array 2.c
+++ Array/set operation in array 2.c
@@ -49,12 +49,39 @@ struct Array *Union(struct Array *Arr1,struct Array *Arr2)
     Arr3->size=12;
     return Arr3;
 }
+
+struct Array *Intersection(struct Array *Arr1,struct Array *Arr2)
+{
+    int i,j,k;
+    i=j=k=0;
+    struct Array *Arr3 =(struct Array *)malloc(sizeof(struct Array));
+    while(i<Arr1->length && j<Arr2->length)
+    {
+        if(Arr1->A[i] < Arr2->A[j])
+        {
+            i++;
+        }
+        else if(Arr1->A[i] > Arr2->A[j])
+        {
+         j++;
+        }
+        else
+        {
+           Arr3->A[k] = Arr2->A[j] ;
+           k++;j++;i++;
+        }
+    }
+    Arr3->length=k;
+    Arr3->size=12;
+    return Arr3;
+}
+
 int main()
 {
     struct Array Arr1 = {{2,5,7,10,13,17},6,10};
     struct Array Arr2 = {{4,5,9,12,17,19},6,10};
     struct Array *Arr3;
-    Arr3 = Union(&Arr1,&Arr2);
+    Arr3 = Intersection(&Arr1,&Arr2);
     print(*Arr3);
     return 0;
 }
