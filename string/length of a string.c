1751198ec1abe28c8f317dd0263d8250d7fd36a2
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-28 12:16:44 +0600

    length of a string

---
+++ string/length of a string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    //char s[20] = "Welcome";
+    char s[100];
+    printf("Enter your string: ");
+    gets(s);
+    int i,count=1;
+    for(i=0;s[i]!='\0';i++)
+    {
+
+    }
+    printf("The input string length is : %d\n",i);
+}
